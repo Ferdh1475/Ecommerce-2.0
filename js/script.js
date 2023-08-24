90ea64b56f8ca345b519262e4358fc551e50c53b
Ferdh1475 <116805659+Ferdh1475@users.noreply.github.com>
2023-08-24 18:47:14 +0100

    first commit - committing all files to repository

---
+++ js/script.js
@@ -0,0 +1,11 @@
+import { Slider } from './component/function.js';
+import { increaseNumber } from './component/function.js';
+import { ready } from './component/function.js';
+import { newDate } from './component/function.js';
+
+window.onload = () =>{
+    Slider();
+    increaseNumber();
+    ready(); 
+    newDate();
+}
